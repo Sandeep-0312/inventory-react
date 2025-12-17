63df8d298871ae54fc527db0f58ff006dde97dda
Sandeep-0312 <sandyvicky0312@gmail.com>
2025-12-18 01:00:04 +0530

    Initial commit: Inventory management frontend

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+
+const api = axios.create({
+  baseURL: API,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
